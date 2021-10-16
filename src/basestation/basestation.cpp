3a4dc8a5ecf40030813587fd51b54f1ee757f33f
Daniel Tierney <daniel@dtierney.dev>
2021-10-15 20:02:39 -0400

    Initialization and CMake Build System (#2)
    * set up cmake and added a nanogui example to the base station
    
    * updated readme with instructions
    
    * added .gitignore
    
    * Update README.md
    
    * fixed a typo and added git submodule instructions
    
    * removed nanogui example program after testing

---
+++ src/basestation/basestation.cpp
@@ -0,0 +1,11 @@
+// This base station placeholder executable is configured to be built with CMake.
+// The NanoGUI and GLFW dependencies can be included in basestation source files without further setup.
+
+ 
+#include <iostream>
+
+
+int main() {
+	std::cout << "Binghamton University Rover Team - BurtOS 2 - Base Station 2\n";
+	return 0;
+}
