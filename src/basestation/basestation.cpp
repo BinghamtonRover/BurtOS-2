37f28c6757a977561325f18789376a1392053567
JA7ja <jacka73@verizon.net>
2021-11-17 20:19:31 -0500

    Fixed main spacing

--- src/basestation/basestation.cpp
+++ src/basestation/basestation.cpp
@@ -4,7 +4,7 @@
 #include <iostream> 
 #include "session.hpp"
 
-int main () {
+int main() {
 	std::cout << "Binghamton University Rover Team - BurtOS 2 - Base Station 2\n";
 	
 	Session basestation = Session();
