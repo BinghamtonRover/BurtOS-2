c3bbfcd52bd9e48189060e6364370f250b95ee8c
Jack Albertson <36114932+JA7ja@users.noreply.github.com>
2021-11-18 19:46:24 -0500

    Session Class and Initial Window (#7)
    * Added initial nanoGUI window and Session class
    
    * Made suggested changes
    
    * Fixed main spacing
    
    * Reorder NanoGUI/GL includes for Windows support
    
    * Adding Apple-specific condition to link OpenGL framework
    
    * Tweak default window behavior
    
    * Convert indentation to tabs
    
    Co-authored-by: Daniel Tierney <daniel@dtierney.dev>
    Co-authored-by: truizbon <truiz1@binghamton.edu>

--- src/basestation/basestation.cpp
+++ src/basestation/basestation.cpp
@@ -1,11 +1,14 @@
 // This base station placeholder executable is configured to be built with CMake.
 // The NanoGUI and GLFW dependencies can be included in basestation source files without further setup.
 
- 
-#include <iostream>
-
+#include <iostream> 
+#include "session.hpp"
 
 int main() {
 	std::cout << "Binghamton University Rover Team - BurtOS 2 - Base Station 2\n";
+	
+	Session basestation;
+	basestation.gui_loop();
+	
 	return 0;
 }
