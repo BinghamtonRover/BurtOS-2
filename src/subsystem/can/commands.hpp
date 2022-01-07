453c3c8ce5d15782ecaece92c75a936fe37d949b
Daniel Tierney <daniel@dtierney.dev>
2022-01-07 14:58:29 -0500

    Add CAN controller and apply optimization (#22)
    * dragged current can_controller files in
    
    * add new CAN controller
    
    * Create CMake build files for rover_can (#12)
    
    * Add CMake build automation for rover_can library
    
    * Add auto-detection to rover_can
    
    * Fix bug where CAN is always enabled
    
    * Fix auto-detection for Linux
    
    * Tweak CMake status message
    
    Co-authored-by: jmcmah10 <jmcmah10@binghamton.edu>

---
+++ src/subsystem/can/commands.hpp
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <cstring>
+
+#ifdef ONBOARD_CAN_BUS
+#include <unistd.h>
+#include <net/if.h>
+#include <sys/ioctl.h>
+#include <sys/socket.h>
+
+#include <linux/can.h>
+#include <linux/can/raw.h>
+#else
+#warning Compiling rover_can in offline mode
+#endif
+
+#include "constants.hpp"
+
+//Overloads for can_send
+int can_send(Node device, Command command, float data);
+
+//Send out a can message
+int can_send(Node device, Command command, int num_bytes, unsigned long data);
+
+//Recieve a message using can_send
+int can_receive(Node device, Command command);
+
+#ifdef ONBOARD_CAN_BUS
+//Create a can frame
+canfd_frame get_can_frame(int modifier, Node device, Command command, int num_bytes, unsigned long data);
+#endif
+
+//Take the first 4 bytes of an unsigned long, and convert them to big endian
+unsigned long get_big_endian_first_half(unsigned long u);
+
+//Can socket
+bool can_open_socket();
+void can_close_socket();
