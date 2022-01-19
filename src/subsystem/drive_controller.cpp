585b8b79a35c73397f32fd327f9fc1e93e50dc23
Renee <rmui10@stuy.edu>
2022-01-19 14:21:42 -0500

    Add safety limits for angle and mps

--- src/subsystem/drive_controller.cpp
+++ src/subsystem/drive_controller.cpp
@@ -9,35 +9,36 @@ void DriveController::halt() {
 	update_target_velocity();
 }
 
-void DriveController::update_target_velocity() { 
-	target_left_speed = target_velocity_rps; 
-	target_right_speed = target_velocity_rps; 
+void DriveController::update_target_velocity() {
+	target_left_speed = target_velocity_rps;
+	target_right_speed = target_velocity_rps;
 
-	if (target_angle < 0) { 
+	if (target_angle < 0) {
 		target_left_speed = target_velocity_rps * (90 + target_angle) / 90;
 	}
-	else if (target_angle > 0) { 
+	else if (target_angle > 0) {
 		target_right_speed = target_velocity_rps * (90 - target_angle) / 90;
 	}
 }
 
-float DriveController::get_target_velocity() { 
+float DriveController::get_target_velocity() {
 	return target_velocity_mps;
 }
 
 void DriveController::update_motor_acceleration() {
 	left_speed = target_left_speed;
-	right_speed = target_right_speed; 
+	right_speed = target_right_speed;
 }
 
 void DriveController::set_forward_velocity(float mps) {
+	mps = fmin(fmax(mps, -MAX_SPEED), MAX_SPEED);
 	target_velocity_mps = mps;
 	target_velocity_rps = (GEARBOX_RATIO * mps) / (static_cast<float>(M_PI) * WHEEL_DIAMETER_METERS);
 	update_target_velocity();
 }
 
 // -90 = sharp left, 0 = straight, 90 = sharp right
-void DriveController::set_steering_angle(float angle) { 
-	target_angle = angle;
+void DriveController::set_steering_angle(float angle) {
+	target_angle = fmin(fmax(angle, -180), 180);
 	update_target_velocity();
 }
--- src/subsystem/drive_controller.hpp
+++ src/subsystem/drive_controller.hpp
@@ -6,6 +6,7 @@ class DriveController {
 	private:
 		constexpr static float GEARBOX_RATIO = 6.923F;
 		constexpr static float WHEEL_DIAMETER_METERS = 0.271F;
+		constexpr static float MAX_SPEED = 6.1F;
 
 		// Desired movement parameters for primary drive mode
 		float target_velocity_mps = 0.0F;
@@ -14,18 +15,18 @@ class DriveController {
 
 		// Goal speeds to reach safely
 		float target_left_speed = 0.0F;
-		float target_right_speed = 0.0F; 
+		float target_right_speed = 0.0F;
 
 		// Actual speeds to be sent to ODrives (revolutions per second)
 		float left_speed = 0.0F;
 		float right_speed = 0.0F;
 
 		// Call whenever target_angle or target_velocity changes
-		void update_target_velocity();		
+		void update_target_velocity();
 	public:
 		void set_forward_velocity(float mps);
 		void set_steering_angle(float angle);
-		
+
 		// Update and apply target speeds and apply acceleration rate limits
 		void update_motor_acceleration();
 		void halt();
