16800fca5805265c9ca2da685d4fa7de9882e3f6
truizbon <truiz1@binghamton.edu>
2022-01-19 13:03:20 -0500

    indent

--- src/basestation/window_module.cpp
+++ src/basestation/window_module.cpp
@@ -6,16 +6,14 @@ WindowModule::WindowModule() {
 }
 
 void WindowModule::create_screen() {
-    screen = new Screen(Vector2i(500, 700), "Window Module Test");
+    screen = new Screen(Vector2i(1280, 750), "Window Module Test");
     screen->set_visible(true);
 }
 
 void WindowModule::add_widget() {
     widget = new FormHelper(screen);
-    ref<Window> window = widget->add_window(Vector2i(10, 10), "I am a widget");
-    
-    widget->add_group("I am a group inside a widget");
-    widget->add_button("I am a button", []() { std::cout << "Button pressed." << std::endl; });
+    ref<Window> window = widget->add_window(Vector2i(10, 10), "Window 1");
+
     window->center();
 }
 
