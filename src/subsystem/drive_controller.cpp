464d0192d112338517311956796aa9fb499e19fc
Renee Mui <33766107+rmui10@users.noreply.github.com>
2022-01-21 16:24:13 -0500

    Add drive controller safety limits and modes (#27)
    * Add safety limits for angle and mps
    
    * Remove unnecessary changes
    
    * Add drive modes
    
    * Make code more concise
    
    * Add check to make sure enum in setter is valid
    
    * Remove changes needed for own laptop
    
    * Change enum to scoped and tweak drive behavior
    
    Co-authored-by: Renee <rmui10@stuy.edu>
    Co-authored-by: Daniel Tierney <daniel@dtierney.dev>

--- src/subsystem/drive_controller.cpp
+++ src/subsystem/drive_controller.cpp
@@ -1,12 +1,14 @@
 #include "drive_controller.hpp"
 
 #include <cmath>
+#include <boost/math/constants/constants.hpp>
 
 void DriveController::halt() {
-	target_left_speed = 0;
-	target_right_speed = 0;
+	target_velocity_mps = 0;
+	target_velocity_rps = 0;
 	target_angle = 0;
 	update_target_velocity();
+	set_drive_mode(DriveMode::NEUTRAL);
 }
 
 void DriveController::update_target_velocity() { 
@@ -26,18 +28,37 @@ float DriveController::get_target_velocity() {
 }
 
 void DriveController::update_motor_acceleration() {
-	left_speed = target_left_speed;
-	right_speed = target_right_speed; 
+	if (get_drive_mode() == DriveMode::NEUTRAL) {
+		left_speed = 0;
+		right_speed = 0;
+	} else {
+		left_speed = target_left_speed;
+		right_speed = target_right_speed;
+	}
 }
 
 void DriveController::set_forward_velocity(float mps) {
+	mps = fmin(fmax(mps, -MAX_SPEED), MAX_SPEED);
 	target_velocity_mps = mps;
-	target_velocity_rps = (GEARBOX_RATIO * mps) / (static_cast<float>(M_PI) * WHEEL_DIAMETER_METERS);
+	// M_PI is technically non-standard C++
+	// When C++20 support becomes ubiquitous, replace Boost with std::numbers::pi_v<float>
+	target_velocity_rps = (GEARBOX_RATIO * mps) / (boost::math::constants::pi<float>() * WHEEL_DIAMETER_METERS);
 	update_target_velocity();
 }
 
 // -90 = sharp left, 0 = straight, 90 = sharp right
 void DriveController::set_steering_angle(float angle) { 
-	target_angle = angle;
+	target_angle = fmin(fmax(angle, -180), 180);
 	update_target_velocity();
+}
+
+DriveController::DriveMode DriveController::get_drive_mode() {
+	return current_mode;
+}
+
+void DriveController::set_drive_mode(DriveMode mode) {
+	if (mode < DriveMode::COUNT) {
+		current_mode = mode;
+		update_motor_acceleration();
+	}
 }
--- src/subsystem/drive_controller.hpp
+++ src/subsystem/drive_controller.hpp
@@ -3,9 +3,24 @@
 #include <cstdint>
 
 class DriveController {
+	public:
+		void set_forward_velocity(float mps);
+		void set_steering_angle(float angle);
+
+		// Update and apply target speeds and apply acceleration rate limits
+		void update_motor_acceleration();
+		void halt();
+		float get_target_velocity();
+
+		enum class DriveMode { NEUTRAL, DRIVE, COUNT };
+
+		DriveMode get_drive_mode();
+		void set_drive_mode(DriveMode mode);
+
 	private:
 		constexpr static float GEARBOX_RATIO = 6.923F;
 		constexpr static float WHEEL_DIAMETER_METERS = 0.271F;
+		constexpr static float MAX_SPEED = 6.1F;
 
 		// Desired movement parameters for primary drive mode
 		float target_velocity_mps = 0.0F;
@@ -22,12 +37,6 @@ class DriveController {
 
 		// Call whenever target_angle or target_velocity changes
 		void update_target_velocity();		
-	public:
-		void set_forward_velocity(float mps);
-		void set_steering_angle(float angle);
 		
-		// Update and apply target speeds and apply acceleration rate limits
-		void update_motor_acceleration();
-		void halt();
-		float get_target_velocity();
+		DriveMode current_mode = DriveMode::NEUTRAL;
 };
