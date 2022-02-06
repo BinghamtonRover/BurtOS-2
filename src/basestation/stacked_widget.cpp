ee1747726a6f4be8f6ed3f5ab6dc2deca025300d
truizbon <truiz1@binghamton.edu>
2022-02-06 14:44:31 -0500

    add necessary stacked_widget class for window manager

---
+++ src/basestation/stacked_widget.cpp
@@ -0,0 +1,58 @@
+/*
+    nanogui/stackedwidget.cpp -- Widget used to stack widgets on top
+    of each other. Only the active widget is visible.
+
+    The stacked widget was contributed by Stefan Ivanov.
+
+    NanoGUI was developed by Wenzel Jakob <wenzel.jakob@epfl.ch>.
+    The widget drawing code is based on the NanoVG demo application
+    by Mikko Mononen.
+
+    All rights reserved. Use of this source code is governed by a
+    BSD-style license that can be found in the LICENSE.txt file.
+*/
+
+#include "stacked_widget.hpp"
+#include <iostream>
+
+NAMESPACE_BEGIN(nanogui)
+
+
+StackedWidget::StackedWidget(nanogui::Widget *parent)
+    : Widget(parent) { }
+
+void StackedWidget::set_selected_index(int index) {
+    if (m_selected_index >= 0)
+        m_children[m_selected_index]->set_visible(false);
+    m_selected_index = index;
+    m_children[m_selected_index]->set_visible(true);
+}
+
+int StackedWidget::selected_index() const {
+    return m_selected_index;
+}
+
+void StackedWidget::perform_layout(NVGcontext *ctx) {
+    for (auto child : m_children) {
+        child->set_position(Vector2i(0));
+        child->set_size(m_size);
+        child->perform_layout(ctx);
+    }
+}
+
+Vector2i StackedWidget::preferred_size(NVGcontext *ctx) const {
+    Vector2i size = Vector2i(0);
+    for (auto child : m_children)
+        size = max(child->preferred_size(ctx), size);
+    return size;
+}
+
+void StackedWidget::add_child(int index, Widget *widget) {
+    if (m_selected_index >= 0)
+        m_children[m_selected_index]->set_visible(false);
+    Widget::add_child(index, widget);
+    widget->set_visible(true);
+    set_selected_index(index);
+}
+
+NAMESPACE_END(nanogui)
