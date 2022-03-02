07b894f54fd3eff142c42493e32fe624cef892fc
Daniel Tierney <daniel@dtierney.dev>
2022-03-02 02:01:44 -0500

    Make functional and style changes to window

--- src/basestation/modules/video_feed_viewer.cpp
+++ src/basestation/modules/video_feed_viewer.cpp
@@ -1,6 +1,7 @@
 #include <modules/video_feed_viewer.hpp>
 
 #include <nanogui/nanogui.h>
+#include <nanogui/opengl.h>
 #include <iostream>
 #include <widgets/layouts/simple_column.hpp>
 #include <widgets/layouts/simple_row.hpp>
@@ -20,13 +21,71 @@ VideoFeedViewer::VideoFeedViewer(nanogui::Widget* parent) :
 {
 	vid_feed = this;
 
-	set_layout(new gui::SimpleColumnLayout(6, 6, 6, gui::SimpleColumnLayout::HorizontalAnchor::STRETCH));
+	set_layout(new gui::SimpleColumnLayout(MARGIN, MARGIN, 6, gui::SimpleColumnLayout::HorizontalAnchor::STRETCH));
 
 	video_widget = new nanogui::ImageView(this);
+	video_widget->set_border_color(nanogui::Color(0, 0, 0, 0));
 	
 	set_size(nanogui::min(nanogui::Vector2i(1280, 720), parent->size() - 100));
 	parent->perform_layout(screen()->nvg_context());
 	center();
+}
+
+bool VideoFeedViewer::keyboard_event(int key, int scancode, int action, int modifiers) {
+	if (gui::Window::keyboard_event(key, scancode, action, modifiers)) {
+		return true;
+	}
+	bool handled = false;
+
+	if (action == GLFW_PRESS && !(modifiers & (GLFW_MOD_CONTROL))) {
+		handled = true;
+		switch (key) {
+			case GLFW_KEY_F:
+				borderless = !borderless;
+				if (gui::SimpleColumnLayout* layout = dynamic_cast<gui::SimpleColumnLayout*>(m_layout.get())) {
+					if (borderless) {
+						layout->set_horizontal_margin(0);
+						layout->set_vertical_margin(0);
+					} else {
+						layout->set_horizontal_margin(MARGIN);
+						layout->set_vertical_margin(MARGIN);
+					}
+					perform_layout(screen()->nvg_context());
+				}
+				break;
+			case GLFW_KEY_T: {
+				// Fit the image in the current size
+				fit_image();
+				break;
+			}
+			case GLFW_KEY_Y: {
+				// Resize window to fit the feed
+				nanogui::Vector2i ideal_size = video_widget->image()->size();
+				if (!borderless) {
+					ideal_size += MARGIN;
+				}
+				ideal_size.y() += m_theme->m_window_header_height;
+
+				auto scr = screen();
+				set_size(nanogui::min(ideal_size, scr->size() - 100));
+				perform_layout(scr->nvg_context());
+
+				fit_image();
+				break;
+			}
+			default:
+				handled = false;
+				break;
+		}
+	}
+
+	return handled;
+}
+
+void VideoFeedViewer::fit_image() {
+	nanogui::Vector2f scale = static_cast<nanogui::Vector2f>(video_widget->size()) / static_cast<nanogui::Vector2f>(video_widget->image()->size());
+	video_widget->set_scale(std::min(scale.x(), scale.y()));
+	video_widget->center();
 }
 
 void VideoFeedViewer::update_frame_STATIC(int stream, net::Frame& frame){
--- src/basestation/modules/video_feed_viewer.hpp
+++ src/basestation/modules/video_feed_viewer.hpp
@@ -9,9 +9,15 @@ class VideoFeedViewer : public gui::Window {
 		static void update_frame_STATIC(int stream, net::Frame& frame);
 		void update_frame(int stream, net::Frame& frame);
 
-		nanogui::Vector2i preferred_size(NVGcontext* ctx) const override;
+		virtual nanogui::Vector2i preferred_size(NVGcontext* ctx) const override;
+		virtual bool keyboard_event(int key, int scancode, int action, int modifiers) override;
+
 	protected:
 		nanogui::ImageView* video_widget;
 	private:
+		constexpr static int MARGIN = 4;
 		Decoder decoder;
+		bool borderless = false;
+
+		void fit_image();
 };
